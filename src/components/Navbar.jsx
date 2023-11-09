0faf17c348ef7f9232da9f89501e42d4b78c8daf
Julien <julien-luc77@gmail.com>
2023-11-09 15:35:04 +0100

    Ajout de la Navbar en responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Logo from '../assets/logo.png'
-import {FaBars, FaTimes} from 'react-icons/fa'
+import {FaBars, FaTimes, FaGithub, FaLinkedin} from 'react-icons/fa'
+import {HiOutlineMail} from 'react-icons/hi'
+import {BsFillPersonLinesFill} from 'react-icons/bs'
 import "./Navbar.scss"
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false)
+  const handleClick = () => setNav(!nav)
   return (
     <div className='content px-4'>
       <div>
@@ -11,7 +15,7 @@ const Navbar = () => {
       </div>
 
       {/*Menu*/}
-      <div>
+      <div className='hidden md:flex'>
           <ul className='menu'>
             <li>Accueil</li>
             <li>À propos</li>
@@ -22,21 +26,29 @@ const Navbar = () => {
       </div>
 
       {/*Menu*/}
-      <div className='hidden'>
-        <FaBars />
+      <div onClick={handleClick} className='md:hidden z-10'>
+        {!nav ? <FaBars /> : <FaTimes />}
       </div>
 
       {/*Menu Mobile*/}
-        <ul className='hidden'>
-          <li>Accueil</li>
-          <li>À propos</li>
-          <li>Skills</li>
-          <li>Projet</li>
-          <li>Contact</li>
+        <ul className={!nav ? 'hidden': 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
+          <li className='py-6 text-4xl'>Accueil</li>
+          <li className='py-6 text-4xl'>À propos</li>
+          <li className='py-6 text-4xl'>Skills</li>
+          <li className='py-6 text-4xl'>Projet</li>
+          <li className='py-6 text-4xl'>Contact</li>
         </ul>
 
       {/*Réseau Social*/}
-      <div className='hidden'></div>
+      <div className='flex fixed flex-col top-[35%] left-0'>
+        <ul>
+          <li className='w-[160px] h-[60px] f'>
+            <a href="/">
+              Linkedin <FaLinkedin size={30} />
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
