4e37827d13c9957de73d439bc38b2c8513c8fda9
Julien <julien-luc77@gmail.com>
2023-11-18 09:37:07 +0100

    WIP

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import HTML from '../assets/html.png'
-import CSS from '../assets/css.png'
-import Sass from '../assets/sass.png'
-import JavaScript from '../assets/javascript.png'
+import HTMLimg from '../assets/html.png'
+import CSSimg from '../assets/css.png'
+import Sassimg from '../assets/sass.png'
+import JavaScriptimg from '../assets/javascript.png'
 import Reactimg from '../assets/react.png'
-import Node from '../assets/node.png'
-import Github from '../assets/github.png'
-import Mongo from '../assets/mongo.png'
+import Nodeimg from '../assets/node.png'
+import Githubimg from '../assets/github.png'
+import Mongoimg from '../assets/mongo.png'
 
 const Skills = () => {
   return (
@@ -18,19 +18,19 @@ const Skills = () => {
         </div>
         <div className=' w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
           <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-            <img className='w-20 mx-auto' src={HTML} alt="Logo HTML" />
+            <img className='w-20 mx-auto' src={HTMLimg} alt="Logo HTML" />
             <p className='my-2'>HTML</p>
           </div>
           <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-            <img className='w-20 mx-auto' src={CSS} alt="Logo CSS" />
+            <img className='w-20 mx-auto' src={CSSimg} alt="Logo CSS" />
             <p className='my-2'>CSS</p>
           </div>
           <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-            <img className='w-20 mx-auto' src={Sass} alt="Logo Sass" />
+            <img className='w-20 mx-auto' src={Sassimg} alt="Logo Sass" />
             <p className='my-2'>SASS</p>
           </div>
           <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-            <img className='w-20 mx-auto' src={JavaScript} alt="Logo JavaScript" />
+            <img className='w-20 mx-auto' src={JavaScriptimg} alt="Logo JavaScript" />
             <p className='my-2'>JAVASCRIPT</p>
           </div>
           <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
@@ -38,15 +38,15 @@ const Skills = () => {
             <p className='my-2'>REACT</p>
           </div>
           <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-            <img className='w-20 mx-auto' src={Node} alt="Logo Node" />
+            <img className='w-20 mx-auto' src={Nodeimg} alt="Logo Node" />
             <p className='my-2'>NODE</p>
           </div>
           <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-            <img className='w-20 mx-auto' src={Github} alt="Logo Github" />
+            <img className='w-20 mx-auto' src={Githubimg} alt="Logo Github" />
             <p className='my-2'>GITHUB</p>
           </div>
           <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-            <img className='w-20 mx-auto' src={Mongo} alt="Logo Mongo" />
+            <img className='w-20 mx-auto' src={Mongoimg} alt="Logo Mongo" />
             <p className='my-2'>MONGO</p>
           </div>
         </div>
