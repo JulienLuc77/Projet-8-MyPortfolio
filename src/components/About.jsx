35bb5349b5b450de3ffb84d2c6bb1ed123f0da48
Julien <julien-luc77@gmail.com>
2023-11-20 09:48:45 +0100

    Modification du CV et des couleurs du texte

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,9 +13,9 @@ function About() {
       </div>
       <div className='presentation'>
         <div className='presentation2'>
-          <p>Bonjour. Moi c'est Julien, merci pour votre temps.</p>
+          <p>"De nature créative, je partage avec fierté un parcours riche en réussites et en apprentissages."</p>
         </div>
-        <div>
+        <div className='text'>
           <p>J'ai parcouru un chemin professionnel me menant à l'acquisition et au développement d'importantes compétences comme, une pleine implication, un apprentissage autodidacte, la curiosité, le travail en équipe et l'investissement professionnel nécessaire pour accomplir parfaitement les missions qui me sont confiées.</p>
           <p>L'apprentissage du développement web a été pour moi au-delà de découvrir un métier passionnant, une source ouvrant une infinité de possibilités d'imaginer, d'inventer, de rêver tout en ayant les outils pour concevoir, créer cet ensemble dans des projets concrets.</p>
           <p> Je suis aujourd'hui sur une voie professionnelle qui m'anime, me passionne et dans laquelle je m'épanouis au quotidien.</p>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -13,7 +13,7 @@ const Skills = () => {
     <div name='skills' className='w-full h-screen bg-[#101728]  text-gray-400 '>
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
         <div>
-          <p className='text-4xl font-bold inline border-b-4 border-teal-400'>Expérience</p>
+          <p className='text-4xl font-bold inline border-b-4 text-gray-200 border-teal-400'>Expérience</p>
           <p className='py-4'>Voici les technologies avec lesquelles j'ai travaillé pour réaliser mes projets personnels et ceux de ma formation :</p>
         </div>
         <div className=' w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
