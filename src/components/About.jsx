2ab675d2c3aa46c98c1ed17cf53f2f6acab992cb
Julien <julien-luc77@gmail.com>
2023-11-22 13:15:01 +0100

    WIP

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,10 +13,10 @@ function About() {
       </div>
       <div className='presentation'>
         <div className='presentation2'>
-          <p className='text-presentation'>"De nature créative, je partage avec fierté un parcours riche en réussites et en apprentissages."</p>
+          <p className='text-presentation'>"De nature créative, j'ai suivi un parcours riche en expériences et en apprentissages."</p>
         </div>
         <div className='text'>
-          <p>J'ai parcouru un chemin professionnel me menant à l'acquisition et au développement d'importantes compétences comme, une pleine implication, un apprentissage autodidacte, la curiosité, le travail en équipe et l'investissement professionnel nécessaire pour accomplir parfaitement les missions qui me sont confiées.</p>
+          <p>Mon chemin professionnel m'a permis d'acquérir et de développer d'importantes compétences telles que le travail en équipe, un apprentissage autodidacte, la curiosité et l'investissement professionnel nécessaire pour accomplir parfaitement les missions qui me sont confiées.</p>
           <p>L'apprentissage du développement web a été pour moi au-delà de découvrir un métier passionnant, une source ouvrant une infinité de possibilités d'imaginer, d'inventer, de rêver tout en ayant les outils pour concevoir, créer cet ensemble dans des projets concrets.</p>
           <p> Je suis aujourd'hui sur une voie professionnelle qui m'anime, me passionne et dans laquelle je m'épanouis au quotidien.</p>
         </div>
